fb149af5cbd2f145f4f5587ee527c19de69609f4
calgor <ahomayoni3@gmail.com>
2024-01-29 13:08:13 +0330

    Add: added the whole thing

--- Student-Version/model.py
+++ Student-Version/model.py
@@ -5,22 +5,28 @@ import torch.nn as nn
 class QNetwork(nn.Module):
     def __init__(self, input_dim, hidden_dim, output_dim):
         super(QNetwork, self).__init__()
-        self.fc1 = None  # TODO
-        self.fc2 = None  # TODO
-        self.fc3 = None  # TODO
-        self.fc4 = None  # TODO
+        self.fc1 = nn.Linear(input_dim, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, hidden_dim)
+        self.fc3 = nn.Linear(hidden_dim, hidden_dim)
+        self.fc4 = nn.Linear(hidden_dim, output_dim)
         self.relu = nn.ReLU()
 
     def forward(self, x):
-        l1 = None  # TODO
-        l2 = None  # TODO
-        l3 = None  # TODO
-        l4 = None  # TODO
+        l1 = self.relu(self.fc1(x))
+        l2 = self.relu(self.fc2(l1))
+        l3 = self.relu(self.fc3(l2))
+        l4 = self.fc4(l3)
         return l4
 
 
 def get_network_input(player, apple):
     proximity = player.get_proximity()
-    x = torch.cat([torch.from_numpy(player.pos).double(), torch.from_numpy(apple.pos).double(),
-                   torch.from_numpy(player.dir).double(), torch.tensor(proximity).double()])
+    x = torch.cat(
+        [
+            torch.from_numpy(player.pos).float(),
+            torch.from_numpy(apple.pos).float(),
+            torch.from_numpy(player.dir).float(),
+            torch.tensor(proximity).float(),
+        ]
+    )
     return x
